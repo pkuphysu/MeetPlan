0c798f6557fe87fff3c24569aa19a592f9a5b831
Rainshaw <598049186@qq.com>
2020-03-03 00:27:54 +0800

    修复meetplan.utils

--- apps/meet_plan/utils.py
+++ apps/meet_plan/utils.py
@@ -1,18 +1,20 @@
 from .models import SemesterDateRange
 from django.utils import timezone
+from django.db.utils import ProgrammingError
 
 
 def get_semester_date_range():
-    semester = SemesterDateRange.objects.last()
-    if semester:
-        start_date = timezone.datetime(semester.start_date.year,
-                                       semester.start_date.month,
-                                       semester.start_date.day,
-                                       tzinfo=timezone.utc)
-        end_date = timezone.datetime(semester.end_date.year,
-                                     semester.end_date.month,
-                                     semester.end_date.day,
-                                     tzinfo=timezone.utc)
-        return [start_date, end_date]
-    else:
+    try:
+        semester = SemesterDateRange.objects.last()
+    except ProgrammingError:
         return [timezone.now(), timezone.now()]
+
+    start_date = timezone.datetime(semester.start_date.year,
+                                   semester.start_date.month,
+                                   semester.start_date.day,
+                                   tzinfo=timezone.utc)
+    end_date = timezone.datetime(semester.end_date.year,
+                                 semester.end_date.month,
+                                 semester.end_date.day,
+                                 tzinfo=timezone.utc)
+    return [start_date, end_date]
