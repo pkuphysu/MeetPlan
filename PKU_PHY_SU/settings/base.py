e2cdeb3491a313e16179d9b2fb3872a42912e52b
Rainshaw <598049186@qq.com>
2020-03-03 12:32:18 +0800

    修复模板目录配置bug

--- PKU_PHY_SU/settings/base.py
+++ PKU_PHY_SU/settings/base.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'PKU_PHY_SU.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, '../templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
