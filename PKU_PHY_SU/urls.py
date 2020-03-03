bbdb777157558bef31f163ae12c55b45d8bd213e
Rainshaw <rxg@live.com>
2020-03-03 21:48:46 +0800

    修复CMSAdmin教师反馈模板bug

--- PKU_PHY_SU/urls.py
+++ PKU_PHY_SU/urls.py
@@ -31,10 +31,7 @@ urlpatterns = [
 
 
 from django.conf import settings
+from django.conf.urls.static import static
 if settings.DEBUG:
-    from django.views.static import serve  # 导入
-    from django.conf.urls import url
-    from django.conf import settings
-    urlpatterns += [
-        url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT})
-    ]
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
