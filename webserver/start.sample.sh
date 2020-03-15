938f7ee38651c0b6c51637f04beebbc4e2d0f9a9
Rainshaw <rxg@live.com>
2020-03-15 18:34:16 +0800

    修复服务器配置文件bug

--- webserver/start.sample.sh
+++ webserver/start.sample.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-NAME="gunicorn_djangopro"    ##定义一个名字，用来作为进程名
+NAME="PKU_PHY_SU"    ##定义一个名字，用来作为进程名
 DIR=/path/to/PKU_PHY_SU    ##你的项目路径
 USER=root   ##启动该项目使用的用户
 GROUP=root ##启动该项目使用的用户的组
 WORKERS=3
 BIND=unix:${DIR}/webserver/gunicorn.sock ##不需要你手工创建该文件！！！这是自动创建的用来通信的套接字
-DJANGO_SETTINGS_MODULE=PKU_PHY_SU.settings ##改成你的项目
+DJANGO_SETTINGS_MODULE=PKU_PHY_SU.settings.base ##改成你的项目
 DJANGO_WSGI_MODULE=PKU_PHY_SU.wsgi ##改成你的项目
 LOG_LEVEL=info
 
