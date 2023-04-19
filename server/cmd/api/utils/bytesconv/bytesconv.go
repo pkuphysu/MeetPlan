531ffec32458ef0cbbeb5532b633d6be2bf096b6
Rainshaw <rxg@live.com>
2023-04-20 00:01:24 +0800

    chore: hertz package tpl
    (cherry picked from commit 4c6264deccb5db50560f85247dbaddcedde7153f)

---
+++ server/cmd/api/utils/bytesconv/bytesconv.go
@@ -0,0 +1,31 @@
+package bytesconv
+
+import (
+	"reflect"
+	"unsafe"
+)
+
+// B2s converts byte slice to a string without memory allocation.
+// See https://groups.google.com/forum/#!msg/Golang-Nuts/ENgbUzYvCuU/90yGx7GUAgAJ .
+//
+// Note it may break if string and/or slice header will change
+// in the future go versions.
+func B2s(b []byte) string {
+	/* #nosec G103 */
+	return *(*string)(unsafe.Pointer(&b))
+}
+
+// S2b converts string to a byte slice without memory allocation.
+//
+// Note it may break if string and/or slice header will change
+// in the future go versions.
+func S2b(s string) (b []byte) {
+	/* #nosec G103 */
+	bh := (*reflect.SliceHeader)(unsafe.Pointer(&b))
+	/* #nosec G103 */
+	sh := (*reflect.StringHeader)(unsafe.Pointer(&s))
+	bh.Data = sh.Data
+	bh.Len = sh.Len
+	bh.Cap = sh.Len
+	return b
+}
