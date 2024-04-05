f2183563ba04b448cee6d0a5df2d9d0e993237b9
Rainshaw <rxg@live.com>
2024-04-05 21:26:56 +0800

    refactor: add system menu

--- web/src/router/modules/admin.ts
+++ web/src/router/modules/admin.ts
@@ -12,43 +12,10 @@ export default [
       {
         path: "/user/list",
         name: "UserList",
-        component: () => import("@/views/admin/user/index.vue"),
+        component: () => import("@/views/admin/user/list/index.vue"),
         meta: {
           icon: "ri:user-settings-line",
           title: $t("menus.userList"),
-          roles: ["admin"],
-          keepAlive: true
-        }
-      },
-      {
-        path: "/user/department",
-        name: "DepartmentList",
-        component: () => import("@/views/admin/department/index.vue"),
-        meta: {
-          icon: "mingcute:department-line",
-          title: $t("menus.departmentList"),
-          roles: ["admin"],
-          keepAlive: true
-        }
-      },
-      {
-        path: "/user/major",
-        name: "MajorList",
-        component: () => import("@/views/admin/major/index.vue"),
-        meta: {
-          icon: "icon-park-outline:degree-hat",
-          title: $t("menus.majorList"),
-          roles: ["admin"],
-          keepAlive: true
-        }
-      },
-      {
-        path: "/user/grade",
-        name: "GradeList",
-        component: () => import("@/views/admin/grade/index.vue"),
-        meta: {
-          icon: "ph:users",
-          title: $t("menus.gradeList"),
           roles: ["admin"],
           keepAlive: true
         }
@@ -66,10 +33,43 @@ export default [
       {
         path: "/system/option",
         name: "OptionList",
-        component: () => import("@/views/admin/option/index.vue"),
+        component: () => import("@/views/admin/system/option/index.vue"),
         meta: {
           icon: "ri:user-settings-line",
           title: $t("menus.optionList"),
+          roles: ["admin"],
+          keepAlive: true
+        }
+      },
+      {
+        path: "/user/department",
+        name: "DepartmentList",
+        component: () => import("@/views/admin/system/department/index.vue"),
+        meta: {
+          icon: "mingcute:department-line",
+          title: $t("menus.departmentList"),
+          roles: ["admin"],
+          keepAlive: true
+        }
+      },
+      {
+        path: "/user/major",
+        name: "MajorList",
+        component: () => import("@/views/admin/system/major/index.vue"),
+        meta: {
+          icon: "icon-park-outline:degree-hat",
+          title: $t("menus.majorList"),
+          roles: ["admin"],
+          keepAlive: true
+        }
+      },
+      {
+        path: "/user/grade",
+        name: "GradeList",
+        component: () => import("@/views/admin/system/grade/index.vue"),
+        meta: {
+          icon: "ph:users",
+          title: $t("menus.gradeList"),
           roles: ["admin"],
           keepAlive: true
         }
